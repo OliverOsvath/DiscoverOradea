8ba83d3a86a1395e8d121e04fe28cc14e0a4c222
bogdan <bogdanhalici1989@gmail.com>
2023-05-11 18:06:18 +0100

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(
-
-        );
+        System.out.println();
+        int a =5;
     }
 }
