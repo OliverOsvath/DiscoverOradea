7d137d9b9ddfdf931b9c5eed04517d0980a95be5
bogdan <bogdanhalici1989@gmail.com>
2023-05-25 16:57:19 +0100

    changes Discover Oradea

--- src/main/java/com/sda/discover/oradea/controller/SearchController.java
+++ src/main/java/com/sda/discover/oradea/controller/SearchController.java
@@ -1,52 +1,47 @@
 package com.sda.discover.oradea.controller;
 
 
+
+import com.sda.discover.oradea.controller.dto.SearchDto;
 import com.sda.discover.oradea.model.Hotel;
 import com.sda.discover.oradea.repository.HotelRepository;
 
+import com.sda.discover.oradea.service.AttractionService;
+import com.sda.discover.oradea.service.HotelService;
+import com.sda.discover.oradea.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+
 @Controller
 public class SearchController {
 
-    @Autowired
-    private final HotelRepository hotelRepository;
 
-    public SearchController(HotelRepository hotelRepository) {
-        this.hotelRepository = hotelRepository;
+    private final HotelService hotelService;
+    private final RestaurantService restaurantService;
+    private final AttractionService attractionService;
+
+    public SearchController(HotelService hotelService, RestaurantService restaurantService, AttractionService attractionService) {
+        this.hotelService = hotelService;
+        this.restaurantService = restaurantService;
+        this.attractionService = attractionService;
     }
 
+    @GetMapping("/search")
+    public String showHotel(@ModelAttribute(name = "searchQuery")SearchDto searchDto, Model model) {
+        //List<Hotel> hotels = hotelService.search(searchDto.getValue());
+        //TODO: restaurant and attractions
 
-    @GetMapping("/hotel")
-    public ModelAndView showHotel(@RequestParam Optional<String> search) {
-        ModelAndView modelAndView = new ModelAndView("hotel/list");
 
-        List<Hotel> listHotels = new ArrayList<>();
 
-        if (search.isPresent()) {
-            try {
-                Optional<Hotel> hotel = hotelRepository.findById(Integer.valueOf(search.get()));
-                if (hotel.isPresent()) {
-                    listHotels.add(hotel.get());
-                }
-            } catch (NumberFormatException nfe) {
-                modelAndView.addObject("incorrect_number", true);
-                System.out.println("Not a valid number");
-            }
-            if (listHotels.isEmpty()) {
-                modelAndView.addObject("empty_result", true);
-            }
-            modelAndView.addObject("search", search.get());
-        } else {
-            listHotels = hotelRepository.findAll();
-        }
-        return modelAndView;
+
+        //model.addAttribute("hotels", hotels);
+        //TODO: restaurant and attractions
+
+        return "searchList";// add correct name
     }
 }
---
+++ src/main/java/com/sda/discover/oradea/controller/dto/SearchDto.java
@@ -0,0 +1,20 @@
+package com.sda.discover.oradea.controller.dto;
+
+public class SearchDto {
+    private String value;
+
+    public SearchDto() {
+    }
+
+    public SearchDto(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
